a82472d1a634698824bf65b90b9498030d7eae5d
GeanMilhore <gean.milhore@gmail.com>
2022-01-13 11:38:48 -0300

    todo candidato service replace

--- src/main/java/br/com/cristal/erp/controller/candidato/CandidatoController.java
+++ src/main/java/br/com/cristal/erp/controller/candidato/CandidatoController.java
@@ -1,10 +1,14 @@
 package br.com.cristal.erp.controller.candidato;
 
 import br.com.cristal.erp.controller.candidato.dto.CandidatoPostRequestBody;
+import br.com.cristal.erp.controller.candidato.dto.CandidatoPutRequestBody;
+import br.com.cristal.erp.controller.candidato.dto.CandidatoResponseBody;
 import br.com.cristal.erp.repository.candidato.CandidatoRepository;
 import br.com.cristal.erp.repository.candidato.model.Candidato;
+import br.com.cristal.erp.service.candidato.CandidatoService;
 import br.com.cristal.erp.service.candidato.mappers.CandidatoMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +20,7 @@ import java.util.List;
 public class CandidatoController {
 
     private final CandidatoRepository candidatoRepository;
+    private final CandidatoService candidatoService;
     private final CandidatoMapper candidatoMapper;
 
     @PostMapping
@@ -54,18 +59,15 @@ public class CandidatoController {
 
     @PutMapping(value = "/{id}")
     // TODO Criar classe CandidatoResponse para evitar devolver Entidade de negocios
-    public ResponseEntity<Candidato> replace(@RequestBody CandidatoPostRequestBody candidatoPostRequestBody, @PathVariable Long id){
+    public ResponseEntity<CandidatoResponseBody> replace(@RequestBody CandidatoPutRequestBody candidatoPutRequestBody, @PathVariable Long id){
 
         // TODO retirar regra de negocio do controler e adicionar ao service
 
-        Candidato savedCandidato = candidatoRepository
-                .findById(id)
-                .orElseThrow(() -> new RuntimeException("Candidato Não Encontrado!")); // TODO CRIAR excecao customizada para o front end
+//        candidatoService.replace(candidatoPutRequestBody);
 
-        Candidato candidatoUpdated = candidatoMapper.mapearTabelaCandidato(candidatoPostRequestBody, savedCandidato);
-        candidatoRepository.save(candidatoUpdated);
+        // TODO CRIAR excecao customizada para o front end
 
-        return ResponseEntity.status(200).body(candidatoUpdated);
+        return ResponseEntity.status(200).body(candidatoService.replace(candidatoPutRequestBody));
     }
 
     @DeleteMapping(value = "/{id}")
