9b153eb219ff7e4dbd767caba0e081dd4e877ec6
GeanMilhore <gean.milhore@gmail.com>
2022-01-13 11:42:42 -0300

    adiciona mÃÂapper responseBody

--- src/main/java/br/com/cristal/erp/service/candidato/mappers/CandidatoMapper.java
+++ src/main/java/br/com/cristal/erp/service/candidato/mappers/CandidatoMapper.java
@@ -1,6 +1,9 @@
 package br.com.cristal.erp.service.candidato.mappers;
 
 import br.com.cristal.erp.controller.candidato.dto.CandidatoPostRequestBody;
+import br.com.cristal.erp.controller.candidato.dto.CandidatoPutRequestBody;
+import br.com.cristal.erp.controller.candidato.dto.CandidatoResponseBody;
+import br.com.cristal.erp.repository.candidato.CandidatoRepository;
 import br.com.cristal.erp.repository.candidato.model.Candidato;
 import org.springframework.stereotype.Service;
 
@@ -22,17 +25,31 @@ public class CandidatoMapper {
         return mappedCandidato;
     }
 
-    public Candidato mapearTabelaCandidato(CandidatoPostRequestBody candidatoPostRequestBody, Candidato savedCandidato){
-        savedCandidato.setIdade(candidatoPostRequestBody.getIdade());
-        savedCandidato.setTrabalha(candidatoPostRequestBody.getTrabalha());
-        savedCandidato.setEstuda(candidatoPostRequestBody.getEstuda());
-        savedCandidato.setHrsDisponiveis(candidatoPostRequestBody.getHrsDisponiveis());
-        savedCandidato.setProgramou(candidatoPostRequestBody.getProgramou());
-        savedCandidato.setClasseSocial(candidatoPostRequestBody.getClasseSocial());
-        savedCandidato.setDescricaoPqMereceQuer(candidatoPostRequestBody.getDescricaoPqMereceQuer());
-        savedCandidato.setMarcarEntrevista(candidatoPostRequestBody.getMarcarEntrevista());
-        savedCandidato.setPeriodoDisponivel(candidatoPostRequestBody.getPeriodoDisponivel());
+    public Candidato mapearTabelaCandidato(CandidatoPutRequestBody candidatoPutRequestBody, Candidato savedCandidato){
+        savedCandidato.setIdade(candidatoPutRequestBody.getIdade());
+        savedCandidato.setTrabalha(candidatoPutRequestBody.getTrabalha());
+        savedCandidato.setEstuda(candidatoPutRequestBody.getEstuda());
+        savedCandidato.setHrsDisponiveis(candidatoPutRequestBody.getHrsDisponiveis());
+        savedCandidato.setProgramou(candidatoPutRequestBody.getProgramou());
+        savedCandidato.setClasseSocial(candidatoPutRequestBody.getClasseSocial());
+        savedCandidato.setDescricaoPqMereceQuer(candidatoPutRequestBody.getDescricaoPqMereceQuer());
+        savedCandidato.setMarcarEntrevista(candidatoPutRequestBody.getMarcarEntrevista());
+        savedCandidato.setPeriodoDisponivel(candidatoPutRequestBody.getPeriodoDisponivel());
 
         return savedCandidato;
+    }
+
+    public CandidatoResponseBody mapearCandidatoResponse(Candidato candidato){
+        CandidatoResponseBody responseBody = new CandidatoResponseBody();
+        responseBody.setIdade(candidato.getIdade());
+        responseBody.setTrabalha(candidato.getTrabalha());
+        responseBody.setEstuda(candidato.getEstuda());
+        responseBody.setHrsDisponiveis(candidato.getHrsDisponiveis());
+        responseBody.setProgramou(candidato.getProgramou());
+        responseBody.setClasseSocial(candidato.getClasseSocial());
+        responseBody.setDescricaoPqMereceQuer(candidato.getDescricaoPqMereceQuer());
+        responseBody.setMarcarEntrevista(candidato.getMarcarEntrevista());
+        responseBody.setPeriodoDisponivel(candidato.getPeriodoDisponivel());
+        return responseBody;
     }
 }
