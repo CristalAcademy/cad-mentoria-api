6eb18147c13db4db2bf6b3467d672e912fae9ad3
GeanMilhore <gean.milhore@gmail.com>
2022-01-13 11:32:13 -0300

    adiciona config securityconfig

--- src/main/java/br/com/cristal/erp/StartApplicationERP.java
+++ src/main/java/br/com/cristal/erp/StartApplicationERP.java
@@ -2,10 +2,14 @@ package br.com.cristal.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
 
 @SpringBootApplication
 public class StartApplicationERP {
     public static void main(String[] args) {
         SpringApplication.run(StartApplicationERP.class, args);
     }
+
 }
