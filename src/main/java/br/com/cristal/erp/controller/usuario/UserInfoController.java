fef7b62bf848e3a568eac0fec682a40027359eff
Bruno Vidal <bruno.vidal@opah.com.br>
2022-02-08 14:37:46 -0300

    add get mapping

--- src/main/java/br/com/cristal/erp/controller/usuario/UserInfoController.java
+++ src/main/java/br/com/cristal/erp/controller/usuario/UserInfoController.java
@@ -4,6 +4,7 @@ import br.com.cristal.erp.controller.usuario.dto.UsuarioResponseBody;
 import br.com.cristal.erp.service.usuario.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class UserInfoController {
 
     private final UsuarioService usuarioService;
 
-    @RequestMapping(value = "/me")
+    @GetMapping(value = "/me")
     public ResponseEntity<UsuarioResponseBody> buscarUsuario() {
 
         return ResponseEntity.ok(usuarioService.buscarUsuario());
