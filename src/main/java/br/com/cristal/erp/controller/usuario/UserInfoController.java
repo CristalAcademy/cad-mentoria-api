011d2f9704f5ae7c0b60d7e41f91ac5debabdefa
Bruno Vidal <bruno.vidal@opah.com.br>
2022-02-08 14:32:27 -0300

    add user infos

---
+++ src/main/java/br/com/cristal/erp/controller/usuario/UserInfoController.java
@@ -0,0 +1,24 @@
+package br.com.cristal.erp.controller.usuario;
+
+import br.com.cristal.erp.controller.usuario.dto.UsuarioResponseBody;
+import br.com.cristal.erp.service.usuario.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+@RequiredArgsConstructor
+public class UserInfoController {
+
+
+    private final UsuarioService usuarioService;
+
+    @RequestMapping(value = "/me")
+    public ResponseEntity<UsuarioResponseBody> buscarUsuario() {
+
+        return ResponseEntity.ok(usuarioService.buscarUsuario());
+
+    }
+}
