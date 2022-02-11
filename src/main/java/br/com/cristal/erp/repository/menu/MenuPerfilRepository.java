f908388decdb962f399b32d7ad9b87e7e7eb5418
GeanMilhore <gean.milhore@gmail.com>
2022-02-11 14:00:30 -0300

    adiciona consulta menu

--- src/main/java/br/com/cristal/erp/repository/menu/MenuPerfilRepository.java
+++ src/main/java/br/com/cristal/erp/repository/menu/MenuPerfilRepository.java
@@ -1,9 +1,14 @@
 package br.com.cristal.erp.repository.menu;
 
 import br.com.cristal.erp.repository.menu.model.menuperfil.MenuPerfil;
+import br.com.cristal.erp.repository.usuario.model.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MenuPerfilRepository extends JpaRepository<MenuPerfil, Long> {
+    Optional<List<MenuPerfil>> findAllByPerfil(Perfil perfil);
 }
--- src/main/java/br/com/cristal/erp/service/menuperfil/MenuPerfilService.java
+++ src/main/java/br/com/cristal/erp/service/menuperfil/MenuPerfilService.java
@@ -1,5 +1,6 @@
 package br.com.cristal.erp.service.menuperfil;
 
+import br.com.cristal.erp.controller.menuperfil.dto.MenuPerfilQueryResponse;
 import br.com.cristal.erp.controller.menuperfil.dto.MenuPerfilRequest;
 import br.com.cristal.erp.controller.menuperfil.dto.MenuPerfilResponse;
 import br.com.cristal.erp.exception.BadRequestsException;
@@ -7,15 +8,19 @@ import br.com.cristal.erp.mapper.MenuPerfilMapper;
 import br.com.cristal.erp.repository.menu.MenuPerfilRepository;
 import br.com.cristal.erp.repository.menu.model.menu.Menu;
 import br.com.cristal.erp.repository.menu.model.menuperfil.MenuPerfil;
+import br.com.cristal.erp.repository.usuario.model.Perfil;
 import br.com.cristal.erp.service.menu.MenuLinker;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class MenuPerfilService {
 
     private MenuPerfilRepository menuPerfilRepository;
+    private MenuPerfilConsulta menuPerfilConsulta;
     private MenuLinker menuLinker;
 
     public void verificaMenu(Long idMenu){
@@ -29,5 +34,9 @@ public class MenuPerfilService {
         MenuPerfil menuPerfilToBeSaved = MenuPerfilMapper.INSTANCE.toMenuPerfil(request);
         MenuPerfil savedMenu = menuPerfilRepository.save(menuPerfilToBeSaved);
         return MenuPerfilMapper.INSTANCE.toMenuPerfilResponse(savedMenu);
+    }
+
+    public List<MenuPerfilQueryResponse> consultar(Perfil perfil) {
+        return menuPerfilConsulta.listarMenus(perfil);
     }
 }
