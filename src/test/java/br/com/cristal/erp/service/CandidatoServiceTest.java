7234eafca6f3d771f10d6721db73a6989ca69008
GeanMilhore <gean.milhore@gmail.com>
2022-03-10 16:24:53 -0300

    adiciona teste de falha com usuario Token e teste de nao permitido

--- src/test/java/br/com/cristal/erp/service/CandidatoServiceTest.java
+++ src/test/java/br/com/cristal/erp/service/CandidatoServiceTest.java
@@ -2,6 +2,7 @@ package br.com.cristal.erp.service;
 
 import br.com.cristal.erp.config.usuarioConfig.CustomUserDetailsService;
 import br.com.cristal.erp.controller.candidato.dto.CandidatoPutRequestBody;
+import br.com.cristal.erp.exception.AcessDeniedException;
 import br.com.cristal.erp.mapper.CandidatoMapper;
 import br.com.cristal.erp.repository.candidato.CandidatoRepository;
 import br.com.cristal.erp.repository.candidato.model.Candidato;
@@ -25,6 +26,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.expression.ExpressionException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
@@ -53,12 +55,14 @@ public class CandidatoServiceTest {
 
     @Test
     public void deveAtualizarCandidatoPeloUsuarioQuandoSucesso(){
-        CandidatoPutRequestBody candidatoPutDto = criarDtoCandidatoPut();
 
+        // instancia oque será usado
+        CandidatoPutRequestBody candidatoPutDto = criarDtoCandidatoPut();
         Usuario mockUsuario = criarUsuario();
         Candidato mockCandidato = criarCandidato();
         Optional<Candidato> candidatoOptional = Optional.of(mockCandidato);
 
+        // define o comportamento dos mocks
         when(mockUsuario.getId()).thenReturn(1L);
         when(mockCandidato.getUsuario()).thenReturn(mockUsuario);
         when(mockCandidato.getId()).thenReturn(candidatoPutDto.getId());
@@ -73,15 +77,63 @@ public class CandidatoServiceTest {
 
         when(candidatoRepository.findById(anyLong())).thenReturn(candidatoOptional);
 
+        // chama o método a ser testado
         candidatoService.replace(candidatoPutDto, 1L);
 
+        // faz as verificações dentro do teste
         verify(candidatoRepository).save(captorCandidato.capture());
         Candidato candidatoSaved = captorCandidato.getValue();
 
-        verify(candidatoRepository, times(1)).save(any(Candidato.class));
+        verify(candidatoRepository, times(1)).save(eq(candidatoSaved));
         Assertions.assertEquals(candidatoSaved.getUsuario(), mockUsuario);
         Assertions.assertEquals(candidatoSaved.getId(), candidatoPutDto.getId());
     }
+
+    @Test
+    public void deveFalharTokenNaoEncontraUsuarioQuandoSucesso(){
+
+        CandidatoPutRequestBody candidatoDto = criarDtoCandidatoPut();
+
+        when(jwtUtility.getEmailFromToken()).thenReturn(candidatoDto.getEmail());
+
+        when(customUserDetailsService.loadUserByEmailAndReturnsUsuario(anyString()))
+                .thenThrow(new UsernameNotFoundException("Usuário Ínvalido!"));
+
+        try{
+            candidatoService.replace(candidatoDto, candidatoDto.getId());
+        }catch (Exception e){
+            Assertions.assertEquals(UsernameNotFoundException.class, e.getClass());
+            Assertions.assertEquals("Usuário Ínvalido!", e.getMessage());
+        }
+
+        verify(jwtUtility, never()).verifyUser(any(), any());
+        verify(candidatoRepository, never()).save(any());
+    }
+
+    @Test
+    public void deveFalharComUsuarioSemPermissaoQuandoSucesso(){
+        CandidatoPutRequestBody candidatoPutDto = criarDtoCandidatoPut();
+        Usuario mockUsuario = criarUsuario();
+
+        when(customUserDetailsService.loadUserByEmailAndReturnsUsuario(anyString()))
+                .thenReturn(mockUsuario);
+
+        when(jwtUtility.getEmailFromToken()).thenReturn(candidatoPutDto.getEmail());
+
+        when(jwtUtility.verifyUser(eq(mockUsuario), anyLong()))
+                .thenThrow(new AcessDeniedException("Você não tem permissão para fazer atualizações nesse candidato"));
+
+        try{
+            candidatoService.replace(candidatoPutDto, candidatoPutDto.getId());
+        } catch (Exception e){
+            Assertions.assertEquals(AcessDeniedException.class, e.getClass());
+            Assertions.assertEquals
+                    ("Você não tem permissão para fazer atualizações nesse candidato", e.getMessage());
+        }
+
+        verify(candidatoRepository, never()).save(any());
+    }
+
 
     public CandidatoPutRequestBody criarDtoCandidatoPut(){
         CandidatoPutRequestBody candidatoPutRequestBody =
@@ -111,14 +163,5 @@ public class CandidatoServiceTest {
 
     public Candidato criarCandidato(){
         return mock(Candidato.class);
-    }
-
-    public CandidatoPutRequestBody criarDtoCandidatoPutVazio(){
-        CandidatoPutRequestBody candidatoPutRequestBody =
-                CandidatoPutRequestBody
-                        .builder()
-                        .id(1L)
-                        .build();
-        return candidatoPutRequestBody;
     }
 }
